c9ae1ed91222d3ae33e074447c11fd12eae6c727
Sofia Katzman <katzmansof@gmail.com>
2023-04-22 23:59:54 -0400

    finished lab

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./components/App";
 
+
+
 ReactDOM.render(
-  <Router>
+
     <App />
-  </Router>,
+  ,
   document.getElementById("root")
 );
